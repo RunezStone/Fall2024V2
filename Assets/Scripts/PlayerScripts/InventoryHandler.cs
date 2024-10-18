d0d0a30e6a6c0328062681789d222a913624ed92
RunezStone <Ysabelle.Trinidad1351@gmail.com>
2024-10-18 06:48:38 -0700

    Inventory and Items
    Made weapons and part of the Inventory

--- Assets/Scripts/PlayerScripts/InventoryHandler.cs
+++ Assets/Scripts/PlayerScripts/InventoryHandler.cs
@@ -1,29 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 // This code is going to have a reference to the UI so it can display the item
 public class InventoryHandler : MonoBehaviour
 {
     private const int INVENTORYSPACE = 4;
+
+    [SerializeField]
+    public Item currentItem;
+    public Image itemImage;
     public Item[] inventory = new Item[INVENTORYSPACE];
+
+    bool canPickUp = false;
+    public GameObject groundItem;
+
+    
+
+    public PlayerMovement playMove;
+
+    public void Awake()
+    {
+        playMove = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    public void PlayerInput()
+    {
+        // swapping item
+        //probably better way but idk yet, get it to work first
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { SwapItem(inventory[0]); }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { SwapItem(inventory[1]); }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { SwapItem(inventory[2]); }           
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { SwapItem(inventory[3]); }
+
+        if (canPickUp && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpItem();
+        }
+           
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Item")
+        {
+            canPickUp = true;
+            groundItem = collision.gameObject;
+            Debug.Log("We can pick up this item!");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        canPickUp = false;
+        groundItem = null;
+    }
 
 
     public void DisplayItem()
     {
-
+        SoundManager.PlaySound(SoundType.SWITCHITEM);
+        itemImage.sprite = currentItem.ItemDisplay;
     }
     public void UseItem()
     {
-
+        currentItem.UseItem();
     }
     public void DropItem()
     {
-
+        Instantiate(currentItem, playMove.rb.transform.position, Quaternion.identity);
+        Destroy(currentItem);
     }
-    public void SwapItem()
+    public void SwapItem(Item newItem)
     {
-
+       if (newItem != null)
+        {
+            currentItem = newItem;
+            Debug.Log("Item Switched to " + currentItem.ItemName);
+            DisplayItem();
+        }
+        else
+        {
+            SoundManager.PlaySound(SoundType.ERROR);
+            Debug.Log("No Item exists");
+        }
+        
     }
+
+    public void PickUpItem()
+    {
+        SwapItem(groundItem.GetComponent<EnemyDrops>().item);
+        Destroy(groundItem);
+    }
+
 }
