2e5f99683e48540772aaaebd7fb053ab50c2af12
RunezStone <Ysabelle.Trinidad1351@gmail.com>
2024-11-15 17:26:58 -0700

    Bullet data

--- Assets/Scripts/ItemScripts/HitData.cs
+++ Assets/Scripts/ItemScripts/HitData.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream:Assets/Scripts/ItemScripts/BulletData.cs
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(CircleCollider2D))]
 public class BulletData : MonoBehaviour
+=======
+[CreateAssetMenu]
+[RequireComponent(typeof(SpriteRenderer))]
+public class HitData : ScriptableObject
+>>>>>>> Stashed changes:Assets/Scripts/ItemScripts/HitData.cs
 {
     private SpriteRenderer sprite;
 
@@ -19,9 +25,17 @@ public class BulletData : MonoBehaviour
         sprite.sprite = bullet;
     }
 
+<<<<<<< Updated upstream:Assets/Scripts/ItemScripts/BulletData.cs
     void OnTriggerEnter2D()
     {
         Destroy(gameObject);
     }
         
+=======
+    public int GetDamage(Type type)
+    {
+
+    }
+
+>>>>>>> Stashed changes:Assets/Scripts/ItemScripts/HitData.cs
 }
